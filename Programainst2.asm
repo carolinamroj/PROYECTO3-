994636d7f6a509e43c0f54f96f431d7af2627095
Irinapsot <ipassot@teleco.upv.es>
2026-01-09 12:07:03 +0100

    Refactor assembly code with additional NOPs

--- Programainst2.asm
+++ Programainst2.asm
@@ -16,10 +16,12 @@ main:
     la x1, input           # guardo en direccion x1 mi input
     lw x1, 0(x1)           # traspaso el valor
     nop
+    nop
     nop                    # evita hazard load-use
 
     la x2, compare_val     # guardo en direccion x2 mi valor de comparacion
     lw x2, 0(x2)           # traspaso el valor
+    nop
     nop
     nop
 
@@ -31,6 +33,7 @@ main:
     # USO DE JALR
     la x3, func_jalr       # llama a func_jalr que calcula input+compare_val y lo guarda en result2
     nop
+    nop
     nop                    # x3 debe estar listo antes de jalr
     jalr x3                # salta a func_jalr
     nop
@@ -38,17 +41,22 @@ main:
 
     # USO DE BLT
     la x3, result3
-    sw x0, 0(x3)           # inicia en 0
     nop
+    nop
+    nop
+    sw x0, 0(x3)           # inicia en 0
 
     blt x1, x2, blt_true   # si input < compare_val salta
     nop
     nop
     j blt_end              # si no se cumple, evita ejecutar el true
     nop
+    nop
 
 blt_true:
     li x4, 1
+    nop
+    nop
     nop
     sw x4, 0(x3)           # si se cumple escribe 1 en result3
 
@@ -56,17 +64,22 @@ blt_end:
 
     # USO DE BLTU
     la x3, result4
-    sw x0, 0(x3)           # inicia en 0
     nop
+    nop
+    nop
+    sw x0, 0(x3)           # inicia en 0
 
     bltu x1, x2, bltu_true # igual que blt pero unsigned
     nop
     nop
     j bltu_end
     nop
+    nop
 
 bltu_true:
     li x4, 1
+    nop
+    nop
     nop
     sw x4, 0(x3)           # si se cumple escribe 1 en result4
 
@@ -74,17 +87,22 @@ bltu_end:
 
     # USO DE BGE
     la x3, result5
-    sw x0, 0(x3)
     nop
+    nop
+    nop
+    sw x0, 0(x3)
 
     bge x1, x2, bge_true   # si input >= compare_val salta
     nop
     nop
     j bge_end
     nop
+    nop
 
 bge_true:
     li x4, 1
+    nop
+    nop
     nop
     sw x4, 0(x3)           # si se cumple escribe 1 en result5
 
@@ -92,17 +110,22 @@ bge_end:
 
     # USO DE BGEU
     la x3, result6
-    sw x0, 0(x3)
     nop
+    nop
+    nop
+    sw x0, 0(x3)
 
     bgeu x1, x2, bgeu_true # igual que bge pero unsigned
     nop
     nop
     j bgeu_end
     nop
+    nop
 
 bgeu_true:
     li x4, 1
+    nop
+    nop
     nop
     sw x4, 0(x3)           # si se cumple escribe 1 en result6
 
@@ -118,7 +141,12 @@ end:
 func_jal:
     add x4, x1, x2         # guarda en x4 input + compare_val
     nop
+    nop
+    nop
     la x5, result1
+    nop
+    nop
+    nop
     sw x4, 0(x5)           # guarda en result1
     nop
     nop
@@ -131,11 +159,15 @@ func_jal:
 func_jalr:
     add x4, x1, x2         # guarda en x4 input + compare_val
     nop
+    nop
+    nop
     la x5, result2
+    nop
+    nop
+    nop
     sw x4, 0(x5)           # guarda en result2
     nop
     nop
     jalr x0, x1, 0         # vuelve al main
     nop
     nop
-
